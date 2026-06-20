3567259219d6637b37be5afdc5931ed1139bb733
pgml <rico@pgml.de>
2026-06-20 14:27:28 +0200

    nana

--- lua/nana/defaults.lua
+++ lua/nana/defaults.lua
@@ -1,5 +1,26 @@
 ---Default colors
 ---@class Colors
 return {
-	bg1 = "#2E3842",
+	bg1          = "#2E3842",
+	bg3          = '#2e353e',
+
+	fg1          = "#d8dee9",
+	fg2          = "#5f666b",
+	fg4          = '#46525c',
+
+	selection_bg = "#3f4750",
+	diff_text    = '#373f48',
+	diff_add     = '#41525a',
+	diff_change  = '#585249',
+	diff_remove  = '#4f434a',
+
+	orange       = '#f97b58',
+	blue1        = "#95b2d6",
+	yellow1      = "#e5cfa9",
+	yellow2      = "#f9ae58",
+	magenta1     = "#c6acc4",
+	red1         = "#ec7f81",
+	red2         = "#ec5f66",
+	green        = "#99c794",
+	cyan1        = "#5fb4b4",
 }
